36bd57ae7eda89715aa8f56e9baab75a95440485
LOADEDDEMO1 <loadeddemo1@gmail.com>
2024-09-02 14:45:23 +0200

    Add files via upload

---
+++ NedbankQuestions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When was Nedbank founded?",
+    answer: "1891",
+    options: [
+      "1888",
+      "1891",
+      "1904",
+      "1912"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the primary color of Nedbank's logo?",
+    answer: "Green",
+    options: [
+      "Pink",
+      "Blue",
+      "Green",
+      "Red"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which country is Nedbank headquartered in?",
+    answer: "South Africa",
+    options: [
+      "Nigeria",
+      "South Africa",
+      "Kenya",
+      "Ghana"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following services is NOT offered by Nedbank?",
+    answer: "Space Travel Insurance",
+    options: [
+      "Personal Banking",
+      "Corporate Banking",
+      "Space Travel Insurance",
+      "Wealth Management"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the slogan of Nedbank?",
+    answer: "See money differently",
+    options: [
+      "Making things happen",
+      "Simplifying your life",
+      "See money differently",
+      "Future-focused banking"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
